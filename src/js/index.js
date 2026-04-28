dcdcbfaba0fe4b4645807b995913817ae57ff878
raileystanford <raileystanford@gmail.com>
2026-04-28 19:13:42 +0300

    Mobile version of Index-about block done

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,46 @@ import {
 // Plugins
 
 // new Parallax();
+
+
+// Functions
+
+function replaceAboutImgMobile() {
+
+  let media = window.matchMedia('(max-width: 996px)').matches;
+
+  if (!media) return;
+
+  let subtitle = document.querySelector('.index-about .heading__subtitle');
+  let imgs = Array.from(document.querySelectorAll('.index-about__pic'));
+
+  if (!subtitle || !imgs) return;
+
+  let elem = document.createElement('div');
+  elem.classList.add('heading__images');
+
+  elem.append(...imgs);
+  subtitle.after(elem);
+
+}
+
+function replaceHeaderImgMobile() {
+
+  let media = window.matchMedia('(max-width: 768px)').matches;
+
+  if (!media) return;
+
+  let subtitle = document.querySelector('.header .heading__subtitle');
+  let img = document.querySelector('.header .food-gravity');
+
+  if (!subtitle || !img) return;
+
+  img.classList.remove('index-intro__food-gravity');
+  img.classList.add('heading__food-gravity');
+  subtitle.after(img);
+
+}
+
+
+replaceHeaderImgMobile();
+replaceAboutImgMobile();
